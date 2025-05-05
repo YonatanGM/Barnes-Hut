b3a025e1500d2bfac1e904496ad3a334dc512135
YonatanGM <girmayonatan86@gmail.com>
2025-05-05 18:50:29 +0200

    Fix binary path

--- scenario_jobs.sh
+++ scenario_jobs.sh
@@ -13,9 +13,9 @@ set -x
 export OMP_NUM_THREADS=48
 
 echo "Running scenario 1..."
-srun --exclusive -N 4 ./simulate --file ../data/state_vectors_csvs/scenario1_19054.csv --dt 1h --t_end 12y --vs 2d --vs_dir sim_s1 --theta 1.05
+srun --exclusive -N 4 ./build/simulate --file ../data/state_vectors_csvs/scenario1_19054.csv --dt 1h --t_end 12y --vs 2d --vs_dir sim_s1 --theta 1.05
 echo "Scenario 1 complete"
 
 echo "Running scenario 2..."
-srun --exclusive -N 4 ./simulate --file ../data/state_vectors_csvs/scenario2_300149.csv --dt 1h --t_end 1y --vs 7d --vs_dir sim_s2 --theta 1.05 --bodies 300000
+srun --exclusive -N 4 ./build/simulate --file ../data/state_vectors_csvs/scenario2_300149.csv --dt 1h --t_end 1y --vs 7d --vs_dir sim_s2 --theta 1.05 --bodies 300000
 echo "Scenario 2 complete"
