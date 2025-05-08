4e7922075afcbff90f0df212a1db46030b497887
YonatanGM <girmayonatan86@gmail.com>
2025-05-08 04:00:53 +0200

    test

--- scenario2.sh
+++ scenario2.sh
@@ -5,13 +5,21 @@
 #SBATCH --output=scenario2_output.out
 #SBATCH --error=scenario2_error.err
 #SBATCH --time=06:30:00
+
 #SBATCH --nodes=4
+#SBATCH --ntasks-per-node=1       # one MPI rank on each node
+#SBATCH --cpus-per-task=48        # allocate all 48 physical cores
 #SBATCH --exclusive
 
 set -e
 set -x
-# export OMP_NUM_THREADS=8
+
+# match OpenMP threads to your allocation
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 echo "Running scenario 2 only... at $(date)"
-srun --exclusive -N 4 ./build/simulate --file ./data/state_vectors_csvs/scenario2_300149.csv --dt 1h --t_end 1y --vs 7d --vs_dir sim_s2 --theta 1.05 --bodies 300000
+srun --cpu-bind=cores ./build/simulate \
+      --file ./data/state_vectors_csvs/scenario2_300149.csv \
+      --dt 1h --t_end 1y --vs 7d --vs_dir sim_s2 \
+      --theta 1.05 --bodies 300000
 echo "Scenario 2 complete at $(date)"
